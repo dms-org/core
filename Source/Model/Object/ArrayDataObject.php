3f8ba100e78ff266806ecbbdb7c6668cce70d63e
Elliot Levin <elliot@aanet.com.au>
2015-10-31 00:00:26 +1100

    Implement array data object, acting as a DTO over a plain array

---
+++ Source/Model/Object/ArrayDataObject.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Iddigital\Cms\Core\Model\Object;
+
+use Iddigital\Cms\Core\Exception\InvalidArgumentException;
+use Iddigital\Cms\Core\Exception\NotImplementedException;
+use Iddigital\Cms\Core\Model\IDataTransferObject;
+use Iddigital\Cms\Core\Model\Type\Builder\Type;
+use Iddigital\Cms\Core\Util\Debug;
+
+/**
+ * The array data transfer object class.
+ *
+ * @author Elliot Levin <elliotlevin@hotmail.com>
+ */
+class ArrayDataObject extends ValueObject implements IDataTransferObject, \ArrayAccess
+{
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * ArrayDataObject constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        parent::__construct();
+        $this->data = $data;
+    }
+
+    /**
+     * Defines the structure of this class.
+     *
+     * @param ClassDefinition $class
+     */
+    protected function define(ClassDefinition $class)
+    {
+        $class->property($this->data)->asArrayOf(Type::mixed());
+    }
+
+    /**
+     * Gets the underlying array.
+     *
+     * @return array
+     */
+    public function getArray()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Returns whether the index exists.
+     *
+     * @param int|string $index
+     *
+     * @return bool
+     */
+    public function offsetExists($index)
+    {
+        return isset($this->data[$index]);
+    }
+
+    /**
+     * Gets the value at the supplied index.
+     *
+     * @param int|string $index
+     *
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function offsetGet($index)
+    {
+        if (!array_key_exists($index, $this->data)) {
+            throw InvalidArgumentException::format(
+                    'Invalid array object index: expecting one of (%s), %s given',
+                    Debug::formatValues(array_keys($this->data)), is_string($index) ? "'{$index}'" : $index
+            );
+        }
+
+        return $this->data[$index];
+    }
+
+    /**
+     * Returns a new array data object with the supplied array data
+     * added to the existing data.
+     *
+     * @param array $data
+     *
+     * @return static
+     */
+    public function with(array $data)
+    {
+        return new ArrayDataObject($data + $this->data);
+    }
+
+    /**
+     * Returns a new array data object with the existing data
+     * without the supplied keys.
+     *
+     * @param array $keys
+     *
+     * @return static
+     */
+    public function without(array $keys)
+    {
+        $keys = array_fill_keys($keys, true);
+
+        return new ArrayDataObject(array_diff_key($this->data, $keys));
+    }
+
+    /**
+     * Not implemented.
+     *
+     * Use the following method instead.
+     *
+     * @see with
+     *
+     * @param mixed $index
+     * @param mixed $value
+     *
+     * @throws NotImplementedException
+     */
+    public function offsetSet($index, $value)
+    {
+        throw NotImplementedException::format(
+                'Invalid call to %s: cannot set array index \'%s\' on class %s as it is immutable, use %s::%s method instead',
+                __METHOD__, $index, __CLASS__, __CLASS__, 'with'
+        );
+    }
+
+    /**
+     * Not implemented.
+     *
+     * Use the following method instead.
+     *
+     * @see without
+     *
+     * @param mixed $index
+     *
+     * @throws NotImplementedException
+     */
+    public function offsetUnset($index)
+    {
+        throw NotImplementedException::format(
+                'Invalid call to %s: cannot unset array index \'%s\' on class %s as it is immutable, use %s::%s method instead',
+                __METHOD__, $index, __CLASS__, __CLASS__, 'without'
+        );
+    }
+}
---
+++ Tests/Tests/Model/Object/ArrayDataObjectTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Iddigital\Cms\Core\Tests\Model\Object;
+
+use Iddigital\Cms\Core\Exception\InvalidArgumentException;
+use Iddigital\Cms\Core\Model\Object\ArrayDataObject;
+
+/**
+ * @author Elliot Levin <elliotlevin@hotmail.com>
+ */
+class ArrayDataObjectTest extends TypedObjectTest
+{
+    /**
+     * @var ArrayDataObject
+     */
+    protected $object;
+
+    /**
+     * @return ArrayDataObject
+     */
+    protected function buildObject()
+    {
+        return new ArrayDataObject([
+                1      => 'abc',
+                'foo'  => 'bar',
+                'null' => null,
+        ]);
+    }
+
+    public function testGetters()
+    {
+        $this->assertSame([
+                1      => 'abc',
+                'foo'  => 'bar',
+                'null' => null,
+        ], $this->object->getArray());
+    }
+
+    public function testArrayAccessGetters()
+    {
+        $this->assertTrue(isset($this->object[1]));
+        $this->assertTrue(isset($this->object['foo']));
+
+        $this->assertFalse(isset($this->object[2]));
+        $this->assertFalse(isset($this->object['abc']));
+        $this->assertFalse(isset($this->object['null']));
+
+        $this->assertSame('abc', $this->object[1]);
+        $this->assertSame('bar', $this->object['foo']);
+        $this->assertSame(null, $this->object['null']);
+
+        $this->assertThrows(function () {
+            $this->object['non-existent'];
+        }, InvalidArgumentException::class);
+    }
+
+    public function testWith()
+    {
+        $newData = $this->object->with([
+                2     => 'def',
+                3     => 123,
+                'foo' => 'baz',
+        ]);
+
+        $this->assertNotEquals($this->object, $newData);
+
+        $this->assertSame([
+                1      => 'abc',
+                'foo'  => 'bar',
+                'null' => null,
+        ], $this->object->getArray());
+
+        $this->assertSame([
+                2     => 'def',
+                3     => 123,
+                'foo' => 'baz',
+                1     => 'abc',
+                'null' => null,
+        ], $newData->getArray());
+    }
+
+    public function testWithout()
+    {
+        $newData = $this->object->without([1, 'null', 'non-existent']);
+
+        $this->assertNotEquals($this->object, $newData);
+
+        $this->assertSame([
+                1      => 'abc',
+                'foo'  => 'bar',
+                'null' => null,
+        ], $this->object->getArray());
+
+        $this->assertSame([
+                'foo'  => 'bar',
+        ], $newData->getArray());
+    }
+}
