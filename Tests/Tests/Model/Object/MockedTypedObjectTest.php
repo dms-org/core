99b47d2330097b1b190f755ce2610f4a5b478a0e
Elliot Levin <elliotlevin@hotmail.com>
2015-12-23 00:08:09 +1100

    Allow typed object to be used a phpunit mocks

---
+++ Tests/Tests/Model/Object/MockedTypedObjectTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dms\Core\Tests\Model\Object;
+
+use Dms\Core\Tests\Model\Object\Fixtures\BlankTypedObject;
+
+/**
+ * @author Elliot Levin <elliotlevin@hotmail.com>
+ */
+class MockedTypedObjectTest extends TypedObjectTest
+{
+    /**
+     * @var BlankTypedObject
+     */
+    protected $object;
+
+    /**
+     * @return BlankTypedObject
+     */
+    protected function buildObject()
+    {
+        return $this->getMock(BlankTypedObject::class);
+    }
+
+    public function testLoads()
+    {
+        $this->assertInstanceOf(BlankTypedObject::class, $this->object);
+        $this->assertInstanceOf(\PHPUnit_Framework_MockObject_MockObject::class, $this->object);
+    }
+}
