655aa4eb55fe54362ad694fbcef6ad8d2db1b375
Elliot Levin <elliotlevin@hotmail.com>
2020-11-14 13:48:42 +1100

    Implement property type detection from property type declarations for PHP >= 7.4

---
+++ tests/Tests/Model/Object/PropertyTypedObjectTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dms\Core\Tests\Model\Object;
+
+use Dms\Common\Testing\CmsTestCase;
+use Dms\Core\Model\Type\Builder\Type;
+use Dms\Core\Tests\Model\Object\Fixtures\TestPropertyTypedObject;
+
+/**
+ * @author Elliot Levin <elliotlevin@hotmail.com>
+ */
+class PropertyTypedObjectTest extends CmsTestCase
+{
+    public function testInfersTypesFromTypeDeclarations()
+    {
+        if (version_compare(PHP_VERSION, '7.4.0', '<=')) {
+            $this->expectNotToPerformAssertions();
+            return;
+        }
+
+        $this->assertEquals([
+            'string' => Type::string(),
+            'int' => Type::int(),
+            'float' => Type::float(),
+            'bool' => Type::bool(),
+            'array' => Type::arrayOf(Type::mixed()),
+            'object' => Type::object(),
+            'iterable' => Type::iterable(),
+            'arrayObject' => Type::object(\ArrayObject::class),
+            'nullableString' => Type::string()->nullable(),
+            'nullableArrayObject' => Type::object(\ArrayObject::class)->nullable(),
+        ], TestPropertyTypedObject::definition()->getPropertyTypeMap());
+    }
+}
